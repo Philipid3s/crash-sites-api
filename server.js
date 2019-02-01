1e69634e230122cbdce4bb3ff7fb6fda467090a3
Philipid3s <julien.regnault@bradyplc.com>
2019-02-01 14:20:43 +0800

    test

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 const routes = require('./src/routes/apiRoutes');
 
 const express = require('express');
@@ -8,9 +9,9 @@ const PORT = process.env.PORT || 3000;
 const DB_URL = process.env.PROD_MONGODB;
 
 // Mongoose connection
-// mongoose.connect(DB_URL, {
-//    useNewUrlParser: true
-//});
+mongoose.connect(DB_URL, {
+    useNewUrlParser: true
+});
 
 // bodyparser setup
 app.use(bodyParser.urlencoded({ extended: true}));
@@ -18,6 +19,9 @@ app.use(bodyParser.json());
 
 // Call routes controller
 routes(app);
+
+// Server static public files
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () =>
     console.log(`Your server is running on port ${PORT}`)
