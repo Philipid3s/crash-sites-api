eb6514be7a9bd311bfb30c1423397b47ce17b36a
Philipid3s <julien.regnault@bradyplc.com>
2019-02-01 14:06:04 +0800

    PROD_MONGODB

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const path = require('path');
 const routes = require('./src/routes/apiRoutes');
 
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
-const DB_URL = process.env.MONGODB_URI || 'mongodb://crash-sites:lotus-1978@ds123181.mlab.com:23181/heroku_215sspjc';
+const DB_URL = process.env.PROD_MONGODB;
 
 // Mongoose connection
 mongoose.connect(DB_URL, {
